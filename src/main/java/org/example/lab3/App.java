db576aec3c7630f7cf357ccfd8968e24c8408e46
Peter Mwendia <ptermwendia@gmail.com>
2023-10-10 22:21:30 +0300

    third commit

--- src/main/java/org/example/lab3/App.java
+++ src/main/java/org/example/lab3/App.java
@@ -1,5 +1,7 @@
 package org.example.lab3;
 
+import java.util.function.Function;
+
 /**
  * Hello world!
  *
@@ -23,6 +25,29 @@ public class App {
         }
     }
 
+    static class IsEven implements Function<Integer, Boolean> {
+        @Override
+        public Boolean apply(Integer x) {
+            return x % 2 == 0;
+        }
+    }
+
+    static class IsDivisibleByThree implements Function<Integer, Boolean> {
+        @Override
+        public Boolean apply(Integer x) {
+            return x % 3 == 0;
+        }
+    }
+
+    public static void printNumbers(int from, int to, Function<Integer, Boolean> filter) {
+        System.out.printf("Printing numbers in the range [%d,%d]\n", from, to);
+        for (int i = from; i <= to; i++) {
+            if (filter.apply(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length < 3) {
             System.out.println("Error: At least three parameters expected, from, to, and base.");
@@ -40,5 +65,17 @@ public class App {
         } else {
             System.out.println("Error: Invalid base. Base should be either 2 or 3.");
         }
+
+        Function<Integer, Boolean> filter;
+        if (base == 2) {
+            filter = new IsEven();
+        } else if (base == 3) {
+            filter = new IsDivisibleByThree();
+        } else {
+            System.out.println("Error: Invalid base. Base should be either 2 or 3.");
+            return;
+        }
+
+        printNumbers(from, to, filter);
     }
 }
