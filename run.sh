44abb2005f8b4662cc723c74356fccc7f8fb0798
Peter Mwendia <ptermwendia@gmail.com>
2023-10-20 15:28:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ mvn clean package
 # Check if the Maven build was successful
 if [ $? -eq 0 ]; then
     # Run the program with the test cases
-    java -jar target/my-java-app.jar 3 20 5
-    java -jar target/my-java-app.jar 3 20 "3,5"
-    java -jar target/my-java-app.jar 3 20 "3v5"
+    java -jar target/lab3-1.0-SNAPSHOT.jar 3 20 5
+    java -jar target/lab3-1.0-SNAPSHOT.jar 3 20 "3,5"
+    java -jar target/lab3-1.0-SNAPSHOT.jar 3 20 "3v5"
 else
     echo "Maven build failed. Please fix any errors before running the program."
 fi
